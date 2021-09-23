a661fd864bd1556d4a326dbd6498deb205d6a0d8
gnarium <80353633+gnarium@users.noreply.github.com>
2021-09-24 00:08:45 +0530

    DOE

--- resources/views/front_end/index.blade.php
+++ resources/views/front_end/index.blade.php
@@ -618,20 +618,20 @@
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="card mb-2">
-                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness1.jpeg')}}" alt="Card image cap">
+                                            <img class="card-img-top" src="{{ asset('front_end/images/Fitness1.jpeg')}}" alt="Card image cap">
                                         </div>
                                     </div>
 
                                     <div class="col-md-4">
                                         <div class="card mb-2">
-                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness2.jpeg')}}" alt="Card image cap" style="height:390px;">
+                                            <img class="card-img-top" src="{{ asset('front_end/images/Fitness2.jpeg')}}" alt="Card image cap" style="height:390px;">
                                             
                                         </div>
                                     </div>
 
                                     <div class="col-md-4">
                                         <div class="card mb-2">
-                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness5.jpeg')}}" alt="Card image cap" style="height:390px;">
+                                            <img class="card-img-top" src="{{ asset('front_end/images/Fitness5.jpeg')}}" alt="Card image cap" style="height:390px;">
                                             
                                         </div>
                                     </div>
@@ -645,7 +645,7 @@
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="card mb-2">
-                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness4.jpeg')}}" alt="Card image cap" style="height:390px;">
+                                            <img class="card-img-top" src="{{ asset('front_end/images/Fitness4.jpeg')}}" alt="Card image cap" style="height:390px;">
                                             
                                         </div>
                                     </div>
