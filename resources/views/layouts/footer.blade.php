4c9820617793b90fcadfa4dea5038d61cb6353e2
gnarium <80353633+gnarium@users.noreply.github.com>
2021-10-16 19:36:09 +0530

    done

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -63,7 +63,7 @@
                 <div className="row">
                     <div className="col-lg-6 col-md-6 col-sm-12">
                         <div class="copyright" >
-                            <p>Copyright &copy; Parth Charitable Trust 2021.</p><a href="https://www.gnariumiqnovative.com">Designed & Developed By Gnarium Iqnovative Pvt. Ltd./a>
+                            <p>Copyright &copy; Parth Charitable Trust 2021.</p><a href="https://www.gnariumiqnovative.com">Designed & Developed By Gnarium Iqnovative Pvt. Ltd.</a>
                         </div>
                          <!-- copyright -->
                     </div>
