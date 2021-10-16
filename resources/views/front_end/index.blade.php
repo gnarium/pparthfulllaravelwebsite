77f1d70ad47f375fae95f17671852c047bc51f46
gnarium <80353633+gnarium@users.noreply.github.com>
2021-10-16 18:22:41 +0530

    done

--- resources/views/front_end/index.blade.php
+++ resources/views/front_end/index.blade.php
@@ -719,20 +719,20 @@ body {font-family: Arial, Helvetica, sans-serif;}
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="card mb-2">
-                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness 1.jpeg')}}" alt="Card image cap">
+                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness1.jpeg')}}" alt="Card image cap">
                                         </div>
                                     </div>
 
                                     <div class="col-md-4">
                                         <div class="card mb-2">
-                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness 2.jpeg')}}" alt="Card image cap" style="height:390px;">
+                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness2.jpeg')}}" alt="Card image cap" style="height:390px;">
                                             
                                         </div>
                                     </div>
 
                                     <div class="col-md-4">
                                         <div class="card mb-2">
-                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness 5.jpeg')}}" alt="Card image cap" style="height:390px;">
+                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness5.jpeg')}}" alt="Card image cap" style="height:390px;">
                                             
                                         </div>
                                     </div>
@@ -746,7 +746,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="card mb-2">
-                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness 4.jpeg')}}" alt="Card image cap" style="height:390px;">
+                                            <img class="card-img-top" src="{{ asset('front_end/images/fitness4.jpeg')}}" alt="Card image cap" style="height:390px;">
                                             
                                         </div>
                                     </div>
