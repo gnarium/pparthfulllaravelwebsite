56195def686a221304fa7fd95771b53fb084543a
gnarium <80353633+gnarium@users.noreply.github.com>
2021-12-09 21:17:07 +0530

    Account details corrected

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -172,6 +172,7 @@ hr {
                             <h1 style="text-align:left">Account's Details </h1>
                             <br/>
                             <div class="clearfix" style="text-align:left;">
+                            <p><b>Bank Name:</b>State Bank Of India</p>
                                 <p><b>Account Number:</b>40089328010</p>
                                 <p><b>IFSC Code:</b>SBIN0012830</p>
                                 <br/>
