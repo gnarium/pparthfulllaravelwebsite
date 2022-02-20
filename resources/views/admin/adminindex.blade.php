1cdc67ced621ea9bc5bd4297f951f15bc811cd68
gnarium <80353633+gnarium@users.noreply.github.com>
2022-02-20 13:09:01 +0530

    done

--- resources/views/admin/adminindex.blade.php
+++ resources/views/admin/adminindex.blade.php
@@ -37,7 +37,17 @@
                 <b class="caret"></b>
             </a>
             <ul class="dropdown-menu extended logout">
-                <li><a href="/admin/login"><i class="fa fa-key"></i> Log Out</a></li>
+                <li>
+                 <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                </li>
             </ul>
         </li>
         <!-- user login dropdown end -->
