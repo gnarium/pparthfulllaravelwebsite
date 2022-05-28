5120a81a3937830acb5e112e362523424306cb71
gnarium <80353633+gnarium@users.noreply.github.com>
2022-05-28 23:31:43 +0530

    add

--- app/Models/gallery.php
+++ app/Models/gallery.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Category;
 class gallery extends Model
 {
     protected  $table='gallery';
+    public function getCat()
+    {
+        return $this->belongsTo(Category::class,'cat_id','id');
+    }
 }
